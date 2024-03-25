5462d31094853f547ff1c4a4658975a98c88c8b4
Jeeruz <jdelacruz.proacc@gmail.com>
2024-03-26 05:36:07 +0800

    basic lang

---
+++ src/components/Simple_Component.jsx
@@ -0,0 +1,11 @@
+export default function Simple_Component() {
+  return (
+    <div>
+      <h1>This is a basic component</h1>
+      <p>
+        Components can only return one element, you can put them inside one
+        element to go around this rule.
+      </p>
+    </div>
+  );
+}
