a0731034f29fc1d0441eba9414c8c20e8195ce56
Jeeruz <jdelacruz.proacc@gmail.com>
2024-03-29 04:47:21 +0800

    added extra css

--- src/components/Description.jsx
+++ src/components/Description.jsx
@@ -1,9 +1,9 @@
 export default function Description({ description }) {
   return (
-    <div class="w3-row-padding">
-      <div class="w3-col m12">
-        <div class="w3-card w3-round w3-white">
-          <div class="w3-container w3-padding">
+    <div className="w3-row-padding">
+      <div className="w3-col m12">
+        <div className="w3-card w3-round w3-white">
+          <div className="w3-container w3-padding">
             <p>
               <i>{description}</i>
             </p>
